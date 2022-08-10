534e2bfe29056cc5b861dafef190492c7a1eab73
ghe0 <gheorghe@gheo.tech>
2022-08-10 23:22:37 +0300

    improved contact page

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -11,37 +11,58 @@ impl Component for Contact {
 
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
-            <div class="container fade-in" style="max-width: 600px; height: 100vh">
-            <div class="columns is-vcentered has-text-centered" style="min-height: 80vh"><div class="column">
-                <div class="subtitle is-4">
-                    {"Contact Information"}
+            <div class="container fade-in" style="max-width: 600px; height: 90vh">
+            <div class="columns is-vcentered" style="min-height: 80vh"><div class="column">
+
+                <div class="columns is-mobile">
+                    <div class="column is-4"></div>
+                    <div class="column">
+                        <div class="subtitle is-4">
+                            {"Contact Information"}
+                        </div>
+                    </div>
                 </div>
 
-                <div class="subtitle is-7 has-text-centered">
-                    {"Due to the advanced bot protection, copy-paste will not work."}
-                </div>
-                <div class="columns is-mobile" style="margin-bottom: 30px">
+
+                <div class="columns is-mobile">
                     <div class="column is-4 has-text-right">{"schedule:"}</div>
-                    <div class="column" style="margin-left: 13px">{"11:00 am - 01:00 am UTC"}</div>
+                    <div class="column">{"11:00 am - 01:00 am UTC"}
+                        <div class="subtitle is-7" style="margin-top: 10px">
+                            {"Available every day of the week during these hours."}
+                        </div>
+                    </div>
                 </div>
                 <div class="columns is-mobile">
-                    <div class="column is-4 has-text-right"
-                        style="margin-top: 10px">{"email:"}</div>
+                    <div class="column is-4 has-text-right">{"email:"}</div>
                     <div class="column">
-                        <img style="max-height: 42px"
+                        <img style="max-height: 42px; margin-left:-7px; margin-top: -10px"
                             src="https://filedn.com/lRvVNpEzu7mVLW5g3Ak9iOk/email.png"/>
+                        <div class="subtitle is-7">
+                            {"Due to the advanced bot protection, copy-paste will not work."}
+                        </div>
                     </div>
                 </div>
                 <div class="columns is-mobile">
-                    <div class="column is-4 has-text-right"
-                        style="margin-top: 10px">{"phone:"}</div>
+                    <div class="column is-4 has-text-right">{"phone:"}</div>
                     <div class="column">
-                        <img style="max-height: 42px;"
+                        <img style="max-height: 42px; margin-left:-7px; margin-top: -10px"
                             src="https://filedn.com/lRvVNpEzu7mVLW5g3Ak9iOk/number.png"/>
+                        <div class="subtitle is-7">
+                            {"Signal, WhatsApp and Telegram are available for this number."}
+                        </div>
                     </div>
                 </div>
-                <div class="subtitle is-7 has-text-centered">
-                    {"Signal, WhatsApp and Telegram are available for this number."}
+
+                <div class="columns is-mobile">
+                    <div class="column is-4 has-text-right"></div>
+                    <div class="column" style="max-width: 400px">
+                        <div class="jobtext">
+                            {"...or just go for "}
+                            <a target="_blank" href="https://www.linkedin.com/in/gun1x/">
+                                {"linkedin"}
+                            </a>
+                        </div>
+                    </div>
                 </div>
 
                 <br/><br/><br/>

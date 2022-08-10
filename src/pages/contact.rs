44a1c33a2e9bf95361d3efe00f742562f2a4a477
ghe0 <gheorghe@gheo.tech>
2022-08-11 00:26:33 +0300

    added flyin effect for contacts page

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -14,27 +14,27 @@ impl Component for Contact {
             <div class="container fade-in" style="max-width: 600px; height: 90vh">
             <div class="columns is-vcentered" style="min-height: 80vh"><div class="column">
 
-                <div class="columns is-mobile">
+                <div class="columns is-mobile flyfromtop">
                     <div class="column is-4"></div>
                     <div class="column">
-                        <div class="subtitle is-4">
+                        <div class="subtitle is-4 flyfromright">
                             {"Contact Information"}
                         </div>
                     </div>
                 </div>
 
 
-                <div class="columns is-mobile">
-                    <div class="column is-4 has-text-right">{"schedule:"}</div>
-                    <div class="column">{"11:00 am - 01:00 am UTC"}
+                <div class="columns is-mobile flyfromtop">
+                    <div class="column is-4 has-text-right flyfromleft">{"schedule:"}</div>
+                    <div class="column flyfromright">{"11:00 am - 01:00 am UTC"}
                         <div class="subtitle is-7" style="margin-top: 10px">
                             {"Available every day of the week during these hours."}
                         </div>
                     </div>
                 </div>
                 <div class="columns is-mobile">
-                    <div class="column is-4 has-text-right">{"email:"}</div>
-                    <div class="column">
+                    <div class="column is-4 has-text-right flyfromleft">{"email:"}</div>
+                    <div class="column flyfromright">
                         <img style="max-height: 42px; margin-left:-7px; margin-top: -10px"
                             src="https://filedn.com/lRvVNpEzu7mVLW5g3Ak9iOk/email.png"/>
                         <div class="subtitle is-7">
@@ -42,9 +42,9 @@ impl Component for Contact {
                         </div>
                     </div>
                 </div>
-                <div class="columns is-mobile">
-                    <div class="column is-4 has-text-right">{"phone:"}</div>
-                    <div class="column">
+                <div class="columns is-mobile flyfrombottom">
+                    <div class="column is-4 has-text-right flyfromleft">{"phone:"}</div>
+                    <div class="column flyfromright">
                         <img style="max-height: 42px; margin-left:-7px; margin-top: -10px"
                             src="https://filedn.com/lRvVNpEzu7mVLW5g3Ak9iOk/number.png"/>
                         <div class="subtitle is-7">
@@ -53,10 +53,10 @@ impl Component for Contact {
                     </div>
                 </div>
 
-                <div class="columns is-mobile">
+                <div class="columns is-mobile flyfrombottom">
                     <div class="column is-4 has-text-right"></div>
                     <div class="column" style="max-width: 400px">
-                        <div class="jobtext">
+                        <div class="jobtext flyfromright">
                             {"...or just go for "}
                             <a target="_blank" href="https://www.linkedin.com/in/gun1x/">
                                 {"linkedin"}
